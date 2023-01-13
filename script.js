0adb28846329931344c2cb580f811de65b5f2297
Sean Jordan <25sjordan@springfield-schools.org>
2023-01-13 15:55:27 +0000

    ready

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+function Hola(){
 var fn = prompt("What is your name?");
 var pass = "Clover";
 var pass2 = "00253347";
@@ -23,4 +25,5 @@ function CONF() {
     alert("DECLINED");
     window.open("https://www.google.com/heart", "_self");
   }
+}
 }
